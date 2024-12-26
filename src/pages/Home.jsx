7460452f87cf77ebb2102c65eb2cbbd6e82573ad
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-12-26 23:11:09 +0600

    responsive design added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,28 @@ import Languages from '../components/Languages';
 import FAQ from '../components/FAQ';
 import Review from '../components/Review';
 import CarusolReview from '../components/CarusolReview';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
+        <div >
             <Banner></Banner>
-            <main>
+            <main className='md:space-y-10 md:mb-5'>
                 <Starts></Starts>
-                <div>
+                <div className='shadow-lg'>
+                    <div >
+                        <h3 className='my-5 py-5 text-center text-xl md:text-3xl font-medium text-orange-400 md:font-bold underline underline-offset-8'>Languages</h3>
+                        <Link to='/find-tutors' className='hover:underline hover:text-orange-300'>Show ALL</Link>
+                    </div>
                     <Languages></Languages>
                 </div>
-                <div className='my-5'>
-                <CarusolReview></CarusolReview>
+                <div className='my-5 shadow-md'>
+                    <div>
+                        <h3 className='my-5 py-5 text-center text-xl md:text-3xl font-medium text-orange-400 md:font-bold underline underline-offset-8'>What Client Says</h3>
+                    </div>
+                    <CarusolReview></CarusolReview>
                 </div>
-                <div className='mb-5'>
+                <div className='mt-5 md:mb-10 md:pb-10 '>
                     <FAQ></FAQ>
                 </div>
 
